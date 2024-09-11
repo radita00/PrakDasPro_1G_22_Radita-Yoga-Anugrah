51a72a7105b0293ee6c7bc1334b286da739bf545
radita00 <radita.yogaanugrah2@gmail.com>
2024-09-11 22:21:58 +0700

    tugas percobaan 3 pertanyaan 2

--- src/Pertemuan2/P2_Percobaan3_Pertanyaan1_22.java
+++ src/Pertemuan2/P2_Percobaan3_Pertanyaan1_22.java
@@ -6,24 +6,16 @@ package Pertemuan2;
 public class P2_Percobaan3_Pertanyaan1_22 {
 
     public static void main(String[] args) {
-        System.out.println("\t\t\t\t\tKARTU RENCANA STUDY (KRS)\n");
-        System.out.println("NIM\t\t\t\t: 24417020099");
-        System.out.println("NAMA\t\t\t\t: RADITA YOGA ANUGRAH");
-        System.out.println("PROGRAM STUDY\t\t\t: Diploma IV Teknik Informatika");
-        System.out.println("KELAS\t\t\t\t: 1G\n");
-        System.out.println("-----------------------------------------------------------------------------------------------------------");
-        System.out.println("|\tKODE MK\t\t|\t\tMATA KULIAH\t\t\t|\tSKS\t|\tJAM\t|");
-        System.out.println("-----------------------------------------------------------------------------------------------------------");
-        System.out.println("|\tRTI241001\t|Pancasila\t\t\t\t\t|\t2\t|\t2\t|");
-        System.out.println("|\tRTI241002\t|Konsep Teknologi Informasi\t\t\t|\t2\t|\t4\t|");
-        System.out.println("|\tRTI241003\t|Critical Thinking dan Problem Solving\t\t|\t2\t|\t4\t|");
-        System.out.println("|\tRTI241004\t|Marematika Dasar\t\t\t\t|\t3\t|\t6\t|");
-        System.out.println("|\tRTI241005\t|Rekayasa Perangkat Lunak\t\t\t|\t2\t|\t4\t|");
-        System.out.println("|\tRTI241006\t|Dasar Pemrograman\t\t\t\t|\t2\t|\t4\t|");
-        System.out.println("|\tRTI241007\t|Praktikum Dasar Pemrograman\t\t\t|\t3\t|\t6\t|");
-        System.out.println("|\tRTI241008\t|Keselamatan dan Kesehatan Kerja\t\t|\t2\t|\t4\t|");
-        System.out.println("-----------------------------------------------------------------------------------------------------------");
-        System.out.println("|\t\t\t|\t\t\tJUMLAH\t\t\t|\t18\t|\t36\t|");
-        System.out.println("-----------------------------------------------------------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi - POLINEMA--");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama\t\t: Radiata Yoga Anugrah");
+        System.out.println("NIM\t\t: 24410702009");
+        System.out.println("Kelas\t\t: 1G");
+        System.out.println("No Absen\t: 22");
+        System.out.println("Prodi\t\t: D-IV Teknik Informatika");
+        System.out.println("Alamat\t\t: Jl. Mayjen Panjaitan Dalam, Kec. Lowokwaru, Kota Malang");
+        System.out.println("No HP\t\t: 082244738716");
+        System.out.println("Hobi\t\t: Bersepeda dan Bermain Game");
     }
 }
