32f15c33b435458f003fd384cca91baef23909da
radita00 <radita.yogaanugrah2@gmail.com>
2024-09-13 20:19:14 +0700

    P2_Percobaan1

---
+++ src/Pertemuan3/Siakad22.java
@@ -0,0 +1,36 @@
+package Pertemuan3;
+
+import java.util.Scanner;
+/**
+ * Siakad22
+ */
+public class Siakad22 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        System.out.print("Masukkan nama: ");
+        nama = sc.nextLine();
+        System.out.print("Masukkan NIM: ");
+        nim = sc.nextLine();
+        System.out.print("Masukkan Kelas: ");
+        kelas = sc.nextLine().charAt(0);
+        System.out.print("Masukkan nomer absen: ");
+        absen = sc.nextByte();
+
+        System.out.print("Masukan nilai kuis: ");
+        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukkan nilai tugas:  ");
+        nilaiTugas = sc.nextDouble();
+        System.out.print("Masikkan nilai ujian: ");
+        nilaiUjian = sc.nextDouble();
+
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+
+        System.out.println("Mahasiswa dengan nama " + nama + "(NIM " + nim + ")" + " Kelas " + kelas + " Nomor absen: " + absen);
+        System.out.println("Nilai Akhir: " + nilaiAkhir);
+    }
+}
