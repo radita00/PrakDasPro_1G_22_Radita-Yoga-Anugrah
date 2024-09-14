cc19e88ae4ae2e2f211e519ac29b5e452c5dd2ab
radita00 <radita.yogaanugrah2@gmail.com>
2024-09-14 07:55:08 +0700

    P3_Percobaan2

---
+++ src/Pertemuan3/Kafe22.java
@@ -0,0 +1,33 @@
+package Pertemuan3;
+
+import java.util.Scanner;
+/**
+ * Kafe22
+ */
+public class Kafe22 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean keanggotaan;
+        int jmlKopi, jmlTeh, jmlRoti;
+        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
+        float diskon = 0.1f;
+
+        System.out.print("Masukkan keanggotaan (true/false): ");
+        keanggotaan = sc.nextBoolean();
+        System.out.print("Masukkan jumlah pembelian kopi: ");
+        jmlKopi = sc.nextInt();
+        System.out.print("Masukkan jumlah pembelian teh: ");
+        jmlTeh = sc.nextInt();
+        System.out.print("Masukkan jumlah pembelian roti: ");
+        jmlRoti = sc.nextInt();
+
+        double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
+        double nominalBayar = totalHarga - (diskon * totalHarga);
+
+        System.out.println("Keanggotaan pelanggan " + keanggotaan);
+        System.out.println("Item pembelian " + jmlKopi + " kopi " + jmlTeh + " teh " + jmlRoti + " roti ");
+        System.out.println("Nominal bayar Rp " + nominalBayar);
+    }
+}
