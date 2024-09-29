55a5f07ccd0c86425f3e5731c6cebd7397d86cd4
radita00 <radita.yogaanugrah2@gmail.com>
2024-09-29 11:27:23 +0700

    P5_Percobaan2

---
+++ src/Pertemuan5/PemilihanSwitch22.java
@@ -0,0 +1,35 @@
+package Pertemuan5;
+
+import java.util.Scanner;
+
+public class PemilihanSwitch22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double angka1, angka2, hasil = 0;
+        char operator;
+
+        System.out.print("Masukan angka 1: ");
+        angka1 = sc.nextDouble();
+        System.out.print("Masukan angka 2: ");
+        angka2 = sc.nextDouble();
+        System.out.println("Masukan oprator (+ - * :)");
+        operator = sc.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                break;
+        }
+        System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
+    }
+}
