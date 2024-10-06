cb5f1fe1835bc15f6a0dabeff1a1ce0aff0abf59
radita00 <radita.yogaanugrah2@gmail.com>
2024-10-06 12:01:39 +0700

    P6_Percobaan1

---
+++ src/Pertemuan6/Pemilihan2_Percobaan122.java
@@ -0,0 +1,25 @@
+package Pertemuan6;
+
+import java.util.Scanner;
+/**
+ * Pemilihan2_Percobaan122
+ */
+public class Pemilihan2_Percobaan122 {
+
+    public static void main(String[] args) {
+        Scanner input22 = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukan tahun: ");
+        tahun = input22.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun kabisat");
+            }
+        }else {
+            System.out.println("Bukan tahun kabisat");
+        }
+    }
+}
