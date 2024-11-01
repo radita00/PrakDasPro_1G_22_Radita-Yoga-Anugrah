136f7a8197a6e18b2ea3301a9d94d661e677320c
radita00 <radita.yogaanugrah2@gmail.com>
2024-11-01 13:20:12 +0700

    P10_Percobaan2

---
+++ src/Pertemuan10/ArrayNilai22.java
@@ -0,0 +1,19 @@
+package Pertemuan10;
+
+import java.util.Scanner;
+
+public class ArrayNilai22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nilaiAkhir [] = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukan niali akhir ke-"+i+" : ");
+            nilaiAkhir[i] = sc.nextInt();
+        } 
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }sc.close();
+    }
+}
