95640523e21be6392bc83c2c584b54ce816161b6
radita00 <radita.yogaanugrah2@gmail.com>
2024-11-01 14:49:23 +0700

    P10_Percobaan2_PertanyaanNo5

--- src/Pertemuan10/ArrayNilai22.java
+++ src/Pertemuan10/ArrayNilai22.java
@@ -8,12 +8,17 @@ public class ArrayNilai22 {
 
         int nilaiAkhir [] = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukan niali akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         } 
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i <  nilaiAkhir.length; i++) {
+           if (nilaiAkhir[i]>70) {
+                System.out.println("Nilai mahasiswa ke-"+i+" lulus!");
+           } else{
+                System.out.println("Nilai mahasiswa ke-"+i+" tidak lulus!");
+            }
+        
         }sc.close();
     }
 }
