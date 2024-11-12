4ccb1d823fb2fc2d958fdb4b1d41e8d5579b84c6
radita00 <radita.yogaanugrah2@gmail.com>
2024-11-12 15:17:32 +0700

    P11_Percobaan2_PertanyaanNo6

--- src/Pertemuan11/BioskopWithScanner22.java
+++ src/Pertemuan11/BioskopWithScanner22.java
@@ -9,6 +9,7 @@ public class BioskopWithScanner22 {
         String [][] penonton = new String[4][2];
 
         while (true) {
+            System.out.println("===== INPUT DATA PENONTON =====");
             System.out.print("Masukkan nama: ");
             nama = sc.nextLine();
             System.out.print("Masukkan baris: ");
@@ -17,7 +18,31 @@ public class BioskopWithScanner22 {
             kolom = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            if (baris >= 0 && baris <= penonton.length && kolom >= 0 && kolom <= penonton[0].length){
+                if (penonton[baris-1][kolom-1]==null) {
+                    penonton[baris-1][kolom-1] = nama;
+                } else {
+                    System.out.println();
+                    System.out.println("Kursi telah terisi, masukkan ulang!");
+                    System.out.println();
+                }
+            } else {
+                System.out.println();
+                System.out.println("Baris/kolom yang di pilih tidak tersedia");
+                System.out.println();
+            }
+
+
+            System.out.println("===== TAMPILAN DAFTAR PENONTON =====");
+            System.out.println();
+            for (int i = 0; i < penonton.length; i++) {
+                System.out.print("Penonton pada baris ke-" + (i + 1) + ": ");
+                for (int j = 0; j < penonton[i].length; j++) {
+                    System.out.print((penonton[i][j] == null ? "***" : penonton[i][j]) + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
             System.out.print("Input penonton lainnya? (y/n): ");
             next = sc.nextLine();
 
@@ -25,5 +50,6 @@ public class BioskopWithScanner22 {
                 break;
             }
         }
+        sc.close();
     }
 }
