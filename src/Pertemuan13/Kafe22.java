76942dcf4a9616b409c11cf498233d8dec0c18d3
radita00 <radita.yogaanugrah2@gmail.com>
2024-11-29 13:29:45 +0700

    P13_Percobaan2

--- src/Pertemuan13/Kafe22.java
+++ src/Pertemuan13/Kafe22.java
@@ -1,7 +1,13 @@
 package Pertemuan13;
 
 public class Kafe22 {
-    public static void main (String[] args){
+    public static void Menu (String namaPelanggan, boolean isMemeber){
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMemeber) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -11,5 +17,8 @@ public class Kafe22 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+    public static void main(String[] args) {
+        Menu("Andi", true);
     }
 }
