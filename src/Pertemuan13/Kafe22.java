87752e6260a30151ddc4bba345d394727a5c5eb6
radita00 <radita.yogaanugrah2@gmail.com>
2024-12-05 17:38:36 +0700

    P13_Percobaan3_PertanyaanNo3

--- src/Pertemuan13/Kafe22.java
+++ src/Pertemuan13/Kafe22.java
@@ -29,12 +29,12 @@ public class Kafe22 {
 
         if (kodePromo.equalsIgnoreCase("diskon50")) {
             System.out.print("Selamat anda mendapatkan diskon sebesar 50%");
-            System.out.print("\nHarga semula: Rp" + hargaTotal);
+            System.out.print("\nHarga menu: Rp" + hargaTotal);
             hargaTotal *= 0.5;
         }else if (kodePromo.equalsIgnoreCase("diskon30")) {
             System.out.print("Selamat anda mendapatkan diskon sebesar 30%");
-            System.out.print("\nHarga semula: Rp" + hargaTotal);
-            hargaTotal *= 0.3;
+            System.out.print("\nHarga menu: Rp" + hargaTotal);
+            hargaTotal *= 0.7;
         }else {
             System.out.print("Kode promo yang anda masukkan invalid");
         }
@@ -42,21 +42,31 @@ public class Kafe22 {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        double totalBayar = 0;
         Menu("Andi", true);
 
+        while (true) {
         System.out.print("\nMasukkan nomor menu yang ingin anda pesan: ");
         int pilihMenu = sc.nextInt();
         System.out.print("Masukkan jumlah item yang ingin anda pesan: ");
         int banyakItem = sc.nextInt();
-
+        
         sc.nextLine();
         System.out.print("Masukkan kode promo: ");
         String kodePromo = sc.nextLine();
-        
-        double totalHarga = hitungTotalHarga22(pilihMenu, banyakItem, kodePromo);
-        
-        System.out.println("\nTotal harga untuk pesanan anda: Rp" + totalHarga);
 
-    }
+
+        double totalDiskon = hitungTotalHarga22(pilihMenu, banyakItem, kodePromo);
+        System.out.println("\nTotal harga untuk pesanan anda: Rp" + totalDiskon);
+        
+        totalBayar += totalDiskon;
+
+        System.out.print("\nApakah ada pesanan yang lain? (y/n)");
+        String next = sc.nextLine();
+        
+        if (next.equalsIgnoreCase("n")) {
+            break;
+        }   
+        } System.out.println("\nTotal yang harus dibayar adalah: Rp" + totalBayar);
+    }   
 }
