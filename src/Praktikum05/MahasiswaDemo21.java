a3f46c2477a53d82babadd60088f10b00e7e408e
radita00 <radita.yogaanugrah2@gmail.com>
2025-03-26 22:11:21 +0700

    Pertemuan7_Percobaan

--- src/Praktikum05/MahasiswaDemo21.java
+++ src/Praktikum05/MahasiswaDemo21.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class MahasiswaDemo21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MahasiswaPrestasi21 list = new MahasiswaPrestasi21();
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
 
-        for (int i = 0; i < 5; i++) {
+        MahasiswaPrestasi21 list = new MahasiswaPrestasi21(jumlah);
+
+        for (int i = 0; i < jumlah; i++) {
             System.out.println("Data mahasiswa ke-" + (i+1));
             System.out.print("NIM\t: ");
             String nim = sc.nextLine();
@@ -23,20 +27,18 @@ public class MahasiswaDemo21 {
             Mahasiswa21 mhs = new Mahasiswa21(nim, nama, kelas, ipk);
             list.tambah(mhs);
         }
-
-        System.out.println("\nData mahasiswa sebelum sorting: ");
         list.tampil();
+        System.out.println("--------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("--------------------------------------");
+        System.out.println("Masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = sc.nextDouble();
 
-        System.out.println("\nData mahasiswa setelah sorting berdasarkan IPK (DESC): ");
-        list.bubbleSort();
-        list.tampil();
-
-        System.out.println("Data yang sudh urut menggunakan SELECTION SORT (ASC)");
-        list.selectionSort();
-        list.tampil();
-
-        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
-        list.insertionSort();
-        list.tampil();
+        System.out.println("Menggunakan binary search");
+        double posisi2 = list.findBinarySearch(cari, 0, jumlah-1);
+        int pss2=(int)posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari,pss2);
     }
 }
--- src/Praktikum05/MahasiswaPrestasi21.java
+++ src/Praktikum05/MahasiswaPrestasi21.java
@@ -1,9 +1,12 @@
 package Praktikum05;
 
 public class MahasiswaPrestasi21 {
-    Mahasiswa21 [] listMhs = new Mahasiswa21[5];
+    Mahasiswa21 [] listMhs;
     int idx;
 
+    MahasiswaPrestasi21(int jumlah) {
+        listMhs = new Mahasiswa21[jumlah];
+    }
     void tambah (Mahasiswa21 m){
         if (idx<listMhs.length) {
             listMhs[idx]=m;
@@ -52,5 +55,46 @@ public class MahasiswaPrestasi21 {
             }
             listMhs[j]=temp;
         }   
+    }
+    int sequentialSearching(Double cari){
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk==cari) {
+                posisi=j;
+                break;
+            }
+        }
+        return posisi;
+    }
+    void tampilPosisi(double x, int pos){
+        if (pos != -1) {
+            System.out.println("Data mahasiswa dengan ipk: " + x + " ditemukan pada index " + pos);
+        } else {
+            System.out.println("Data " + x +" tidak ditemukan");
+        }
+    }
+    void tampilDataSearch(double x, int pos){
+        if (pos != -1) {
+            System.out.println("nim\t : "+ listMhs[pos].nim);
+            System.out.println("nama\t : "+listMhs[pos].nama);
+            System.out.println("kelas\t : " + listMhs[pos].kelas);
+            System.out.println("ipk\t : " + listMhs[pos].ipk);
+        } else{
+            System.out.println("Data mahasiswa dengan IPK " + x + " tidak ditemukan");
+        }
+    }
+    int findBinarySearch(double cari, int left, int right){
+        int mid;
+        if (right>=left) {
+            mid = (right+left)/2;
+            if (cari == listMhs[mid].ipk) {
+                return(mid);
+            } else if (listMhs[mid].ipk<cari) {
+                return findBinarySearch(cari, left, mid -1);
+            }else{
+                return findBinarySearch(cari, mid+1, right);
+            }
+        }
+        return -1;
     }
 }
