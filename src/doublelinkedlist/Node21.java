2a98a32d8ec9936017cdd003e2e86153df8efb12
radita00 <radita.yogaanugrah2@gmail.com>
2025-05-22 00:04:27 +0700

    Jobsheet12_Percobaan1

---
+++ src/doublelinkedlist/Node21.java
@@ -0,0 +1,12 @@
+package doublelinkedlist;
+
+public class Node21 {
+    int data;
+    Node21 prev, next;
+
+    Node21 (Node21 prev, int data, Node21 next){
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
