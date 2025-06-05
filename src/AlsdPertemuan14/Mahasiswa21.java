5d1f005b724d91b528b0c1c9ca6a48b7902aff90
radita00 <radita.yogaanugrah2@gmail.com>
2025-06-05 10:10:34 +0700

    Jobsheet14_Percobaan1

---
+++ src/AlsdPertemuan14/Mahasiswa21.java
@@ -0,0 +1,23 @@
+package AlsdPertemuan14;
+
+public class Mahasiswa21 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa21 (){
+    }
+
+    public Mahasiswa21(String nim, String nama, String kelas, double ipk){
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    public void tampilInformasi(){
+        System.out.println("NIM: " + this.nim + " Nama: " + this.nama + " Kelas: " + this.kelas + " IPK: " + this.ipk);
+    }
+
+}
