4431ec26b66c5da8ce3f12900378cfdd68abaa81
radita00 <radita.yogaanugrah2@gmail.com>
2025-06-12 09:11:34 +0700

    JobsheetXIV_Persiapan

---
+++ src/AlsdPraktikum14/Customer21.java
@@ -0,0 +1,19 @@
+package AlsdPraktikum14;
+
+public class Customer21 {
+    public int id;
+    public String name;
+
+    public Customer21(){
+
+    }
+
+    public Customer21(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString(){
+        return "ID: " + this.id + " Nama: " + this.name;
+    }
+}
